5d7afaa5960c37c8c3b61b399afd1a10dff38ef9
Chris Hourihan <chourihan@gmail.com>
2014-10-03 11:10:29 -0400

    removing loader verification

--- controllers/index.js
+++ controllers/index.js
@@ -1,9 +1,5 @@
 module.exports = function routeHandler(app) {
   app.use('/', require('./main'));
   app.use('/api', require('./api'));
-
-  app.get('/loaderio-da47e3a485e284d04007d1ab319cee87', function(req, res) {
-    res.send('loaderio-da47e3a485e284d04007d1ab319cee87');
-  });
 };
 
