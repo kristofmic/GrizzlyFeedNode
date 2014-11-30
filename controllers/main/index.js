e5be2ab3d3e11064b990f3e4167fcb64148f619a
Chris Hourihan <chourihan@gmail.com>
2014-11-30 13:19:31 -0500

    adding healthcheck; adding travis file

--- controllers/main/index.js
+++ controllers/main/index.js
@@ -3,9 +3,14 @@ var
   router = express.Router();
 
 router.get('/', get);
+router.get('/health', health);
 
 module.exports = router;
 
 function get(req, res) {
   res.render('main/index');
+}
+
+function health(req, res) {
+  res.send(200, {});
 }
