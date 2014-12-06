44564f065c7fa0556e6982bc40d9aca1fcb1340f
Chris Hourihan <chourihan@gmail.com>
2014-12-05 21:34:09 -0500

    deploy5

--- tasks/travis/deployment.sh
+++ tasks/travis/deployment.sh
@@ -17,9 +17,9 @@ then
   SERVER_HOST="54.68.226.195"
   SSH_FLAG=" -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no "
   echo "Pushing changes to $SERVER_HOST"
-  rsync -azhe ssh $SSH_FLAG -i ./tasks/travis/aws.pem ./ ec2-user@$SERVER_HOST:~/$PACKAGE/
+  rsync -azhe "ssh $SSH_FLAG -i ./tasks/travis/aws.pem" ./ ec2-user@$SERVER_HOST:~/$PACKAGE/
   echo "Restarting server..."
-  ssh $SSH_FLAG -ti ./tasks/travis/aws.pem ec2-user@$SERVER_HOST 'sudo touch ~/GrizzlyFeedNode/tmp/restart.txt'
+  ssh $SSH_FLAG -ti ./tasks/travis/aws.pem ec2-user@$SERVER_HOST "sudo touch ~/GrizzlyFeedNode/tmp/restart.txt"
   echo "Complete."
   exit 0
 fi
